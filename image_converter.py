20a767dbda1900b20898a97b19c75e6978389d38
Patou <75997617+xorblo-doitus@users.noreply.github.com>
2026-07-01 19:46:29 +0200

    feat: Print a little bit of information at least

--- image_converter.py
+++ image_converter.py
@@ -8,5 +8,12 @@ parser.add_argument('input', help='The image to convert')
 parser.add_argument('-o', '--output', default="*.ico", help='Output FileName. Use * to capture input name.')
 args = parser.parse_args()
 
-img = PIL.Image.open(args.input)
-img.save(args.output.replace("*", pathlib.Path(args.input).stem))
+input: str = args.input
+output: str = args.output.replace("*", pathlib.Path(args.input).stem)
+
+print(f"Converting `{input}` to `{output}`...")
+
+img = PIL.Image.open(input)
+img.save(output)
+
+print(f"Success.")
